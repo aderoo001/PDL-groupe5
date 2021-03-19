634b1ff45dfc7025361d3cdf65d4b78f52a9c9ef
Aggreas <ruppemaxime@orange.fr>
2021-03-19 16:47:53 +0100

    modif controller et image pour infos en plus

--- backend/src/main/java/pdl/backend/Image.java
+++ backend/src/main/java/pdl/backend/Image.java
@@ -3,28 +3,49 @@ package pdl.backend;
 public class Image {
   private static Long count = Long.valueOf(0);
   private final Long id;
+  private long size;
   private String name;
+  private String format;
   private final byte[] data;
 
+  //constructor
   public Image(final String name, final byte[] data) {
     id = count++;
     this.name = name;
     this.data = data;
   }
 
+  //getters
   public long getId() {
     return id;
+  }
+
+  public long getSize() {
+    return size;
   }
 
   public String getName() {
     return name;
   }
 
-  public void setName(final String name) {
-    this.name = name;
+  public String getFormat() {
+    return format;
   }
 
   public byte[] getData() {
     return data;
+  }
+
+  //setters
+  public void setSize(final long size) {
+    this.size = size;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public void setFormat(final String format) {
+    this.format = format;
   }
 }
--- backend/src/main/java/pdl/backend/ImageController.java
+++ backend/src/main/java/pdl/backend/ImageController.java
@@ -1,18 +1,24 @@
 package pdl.backend;
 
-import java.io.IOException;
-
 import java.util.Optional;
 
 import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+
+import org.apache.commons.io.FilenameUtils;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
-import java.awt.image.BufferedImage;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.commons.io.FilenameUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -25,10 +31,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
 
 @RestController
 public class ImageController {
@@ -80,6 +82,7 @@ public class ImageController {
 
     this.imageDao.retrieveAll().forEach(img -> {
       ObjectNode n = mapper.createObjectNode();
+
       //Identifiant
       n.put("id", img.getId());
 
@@ -90,8 +93,8 @@ public class ImageController {
       int height = 0;
       int width = 0;
       InputStream in = new ByteArrayInputStream(img.getData());
-
       BufferedImage buf;
+
       try {
         buf = ImageIO.read(in);
         height = buf.getHeight();
@@ -101,11 +104,13 @@ public class ImageController {
       }
 
       n.put("size", height*width);
-
-      //n.put("poids", img.getData().length);
+      img.setSize(height*width);
 
       //format
-      n.put("format", FilenameUtils.getExtension(img.getName()));
+      String format = FilenameUtils.getExtension(img.getName());
+      
+      n.put("format", format);
+      img.setFormat(format);
 
       //url
       n.put("url", "http://localhost:8080/images/"+img.getId());
@@ -116,53 +121,3 @@ public class ImageController {
     return nodes;
   }
 }
-
-/*
-      File file = new File("images/"+img.getId());
-      String path = file.getAbsolutePath();
-      System.out.println(path);
-
-      final ArrayImgFactory<UnsignedByteType> factory = new ArrayImgFactory<>(new UnsignedByteType());
-		  final ImgOpener imgOpener = new ImgOpener();
-		  final Img<UnsignedByteType> input = (Img<UnsignedByteType>) imgOpener.openImgs(path, factory).get(0);
-		  imgOpener.context().dispose();
-
-      int iw = (int) input.max(0);
-		  int ih = (int) input.max(1);
-      System.out.println(iw);
-      System.out.println(ih);
-*/
-
-/*
-      int height = 0;
-      int width = 0;
-      InputStream in = new ByteArrayInputStream(img.getData());
-
-      BufferedImage buf;
-      try {
-        buf = ImageIO.read(in);
-        height = buf.getHeight();
-        width = buf.getWidth();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-
-      n.put("size", height*width);
-*/
-
-/*
-import net.imglib2.RandomAccess;
-import net.imglib2.Cursor;
-import net.imglib2.img.Img;
-import net.imglib2.img.array.ArrayImgFactory;
-import io.scif.SCIFIO;
-import io.scif.img.ImgIOException;
-import io.scif.img.ImgOpener;
-import io.scif.img.ImgSaver;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
-import net.imglib2.exception.IncompatibleTypeException;
-import java.io.File;
-import net.imglib2.view.Views;
-import net.imglib2.view.IntervalView;
-import net.imglib2.loops.LoopBuilder;
-*/
