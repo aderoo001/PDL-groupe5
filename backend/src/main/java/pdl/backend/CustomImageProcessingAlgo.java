8ee4ca91ff3f4e097e6c0a91239f6fcf14e76ba5
Aggreas <ruppemaxime@orange.fr>
2021-04-16 14:08:27 +0200

    modifs get + custom

---
+++ backend/src/main/java/pdl/backend/CustomImageProcessingAlgo.java
@@ -0,0 +1,13 @@
+package pdl.backend;
+
+import java.util.*;
+import java.util.ArrayList;
+
+public class CustomImageProcessingAlgo {
+    //private ArrayList<Map<String, String>> algorithmList;
+    private Map<String,String> algorithmList;
+
+    public Map<String,String> getAlgorithmList() {
+        return algorithmList;
+    }
+}
